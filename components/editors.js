6b9677c690e0c3be73aed650d14715be661dbc57
Ali Fadda <silvertechguy@gmail.com>
2020-11-02 08:25:00 -0800

    fixing all the typo issues related to react ace ace editor

--- components/editors.js
+++ components/editors.js
@@ -1,31 +1,38 @@
 import React from 'react'
-import AceEditor from 'react-ace'
-import 'ace-builds/src-noconflict/mode-javascript'
-import 'ace-builds/src-noconflict/mode-html'
-import 'ace-builds/src-noconflict/mode-css'
-import 'ace-builds/src-noconflict/theme-monokai'
 
 import styles from './editors.module.css'
 
-const Editor = ({ mode, onChange, value, title, height }) => (
-  <div className={styles.editorContainer}>
-    <div className={styles.editorTitle}>{title}</div>
-    <AceEditor
-      mode={mode}
-      theme="monokai"
-      name={title}
-      onChange={onChange}
-      fontSize={18}
-      width={'100%'}
-      height={height}
-      value={value}
-      showPrintMargin={true}
-      showGutter={true}
-      tabSize={2}
-      setOptions={{ useWorker: false }}
-    />
-  </div>
-)
+const Editor = ({ mode, onChange, value, title, height }) => {
+  if (typeof window !== 'undefined') {
+    const AceEditor = require('react-ace').default
+    require('ace-builds/src-noconflict/mode-javascript')
+    require('ace-builds/src-noconflict/mode-html')
+    require('ace-builds/src-noconflict/mode-css')
+    require('ace-builds/src-noconflict/theme-monokai')
+
+    return (
+      <div className={styles.editorContainer}>
+        <div className={styles.editorTitle}>{title}</div>
+        <AceEditor
+          mode={mode}
+          theme="monokai"
+          name={title}
+          onChange={onChange}
+          fontSize={18}
+          width={'100%'}
+          height={height}
+          value={value}
+          showPrintMargin={true}
+          showGutter={true}
+          tabSize={2}
+          setOptions={{ useWorker: false }}
+        />
+      </div>
+    )
+  }
+
+  return null
+}
 
 export const HtmlEditor = (props) => (
   <Editor title={'HTML'} mode="html" {...props} />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import SplitPane from 'react-split-pane'
 
 import { CssEditor, HtmlEditor, JavascriptEditor } from '../components/editors'
