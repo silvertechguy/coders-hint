bd1d5712ef56e10ebb419dea646aaca3ce9bc446
Ali Fadda <silvertechguy@gmail.com>
2020-11-02 07:58:21 -0800

    finsh preview yab

--- components/Editors.js
+++ components/Editors.js
@@ -1,28 +1,28 @@
 import React from 'react'
-
 import AceEditor from 'react-ace'
-import 'ace-builds'
+import 'ace-builds/src-noconflict/mode-javascript'
 import 'ace-builds/src-noconflict/mode-html'
 import 'ace-builds/src-noconflict/mode-css'
-import 'ace-builds/src-noconflict/mode-javascript'
 import 'ace-builds/src-noconflict/theme-monokai'
 
 import styles from './editors.module.css'
 
-const Editor = ({ title, mode, height }) => (
+const Editor = ({ mode, onChange, value, title, height }) => (
   <div className={styles.editorContainer}>
     <div className={styles.editorTitle}>{title}</div>
     <AceEditor
       mode={mode}
       theme="monokai"
       name={title}
+      onChange={onChange}
       fontSize={18}
       width={'100%'}
+      height={height}
+      value={value}
       showPrintMargin={true}
       showGutter={true}
       tabSize={2}
       setOptions={{ useWorker: false }}
-      height={height}
     />
   </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,38 @@ import React, { useState } from 'react'
 import SplitPane from 'react-split-pane'
 
 import { CssEditor, HtmlEditor, JavascriptEditor } from '../components/editors'
+import { useDebounce } from '../utils/useDebounce'
+
+import styles from './index.module.css'
 
 const Index = () => {
   // Hold the height value in the state, begin with the height of 485px
   const [heightValue, setHeightValue] = useState('485px')
+
+  const [htmlValue, setHtmlValue] = useState('')
+  const [cssValue, setCssValue] = useState('')
+  const [jsValue, setJsValue] = useState('')
+  const [outputValue, setOutputValue] = useState('')
+
+  const debouncedHtml = useDebounce(htmlValue, 1000)
+  const debouncedJs = useDebounce(jsValue, 1000)
+  const debouncedCss = useDebounce(cssValue, 1000)
+
+  useEffect(() => {
+    const output = `<html>
+                      <style>
+                        ${debouncedCss}
+                      </style>
+                      <body>
+                        ${debouncedHtml}
+                        <script type="text/javascript">
+                        ${debouncedJs}
+                        </script>
+                      </body>
+                    </html>`
+
+    setOutputValue(output)
+  }, [debouncedHtml, debouncedCss, debouncedJs])
 
   return (
     <SplitPane
@@ -17,13 +45,25 @@ const Index = () => {
       }}
     >
       <SplitPane split="vertical" minSize={'33%'}>
-        <HtmlEditor height={heightValue} />
+        <HtmlEditor
+          height={heightValue}
+          value={htmlValue}
+          onChange={setHtmlValue}
+        />
         <SplitPane split="vertical" minSize={'50%'}>
-          <CssEditor height={heightValue} />
-          <JavascriptEditor height={heightValue} />
+          <CssEditor
+            height={heightValue}
+            value={cssValue}
+            onChange={setCssValue}
+          />
+          <JavascriptEditor
+            height={heightValue}
+            value={jsValue}
+            onChange={setJsValue}
+          />
         </SplitPane>
       </SplitPane>
-      <div>Preview Page</div>
+      <iframe srcDoc={outputValue} className={styles.previewIframe} />
     </SplitPane>
   )
 }
